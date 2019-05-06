6e00c51968f1a303613134246fffcce9c432590a
Kelley Chaplain <kmchaplain@gmail.com>
2019-05-06 04:15:23 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,4 +3,11 @@ require 'pry'
 def alphabetize(arr)
   espy = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   binding.pry
+  
+  arr.sort_by do |word|
+  word.split('').map do |letter|
+    ESPERANTO_ALPHABET.index(letter)
+  end
+end
+  
 end
